73806a84f9de39fd7b36d6f790f75af74b4b2b9f
Jessica <jessica.costa@justdigital.com.br>
2019-03-15 19:35:26 -0300

    Webpack and routes configurations

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='About' small='Us'></PageHeader>
+    <div>
+      Cat ipsum dolor sit amet, meowzer scratch leg;
+      meow for can opener to feed me cats are fats i like to
+      pets them they like to meow back and cats are cute.
+      Meow to be let in find something else more interesting, 
+      or find a way to fit in tiny box warm up laptop with butt 
+      lick butt fart rainbows until owner yells pee in litter box
+      hiss at cats kitty power fight an alligator and win. 
+      Ignore the squirrels, you'll never catch them anyway stand 
+      in doorway, unwilling to chose whether to stay in or go out, 
+      lick butt decide to want nothing to do with my owner today. 
+      Cough hairball, eat toilet paper i hate cucumber pls dont throw 
+      it at me, but under the bed licks paws yet ccccccccccccaaaaaaaaaaaaaaatttttttttttttttttssssssssssssssss 
+      so find something else more interesting. 
+    </div>
+  </div>
+)
